0f309e40114a8815a9bdbb49441ff7927aee81a1
ngragas <ngragas@student.21-school.ru>
2020-12-29 22:20:11 +0300

    parsing done

--- includes/ft_printf.h
+++ includes/ft_printf.h
@@ -6,18 +6,40 @@
 /*   By: ngragas <ngragas@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/12/03 21:34:53 by ngragas           #+#    #+#             */
-/*   Updated: 2020/12/28 22:49:18 by ngragas          ###   ########.fr       */
+/*   Updated: 2020/12/29 22:03:56 by ngragas          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef FT_PRINTF_H
 # define FT_PRINTF_H
 
-#include "libft.h"
+# include "libft.h"
 
-#include <unistd.h>
-#include <stdarg.h>
+# include <unistd.h>
+# include <stdarg.h>
 
-int ft_printf(const char *format, ...);
+int					ft_printf(const char *format, ...);
+
+/*
+** length: 'hh' = -2; 'h' = -1; 'l' = 1; 'll' = 2
+**	flags:	'-'	= 1		'+'	= 2		' '	= 4		'0'	= 8		'#'	= 16
+**				= 00001		= 00010		= 00100		= 01000		= 10000
+*/
+
+# define FLAG_MINUS	1
+# define FLAG_PLUS	2
+# define FLAG_SPACE	4
+# define FLAG_ZERO	8
+# define FLAG_HASH	16
+
+typedef struct		s_specs
+{
+	unsigned char	flags;
+	int				width;
+	int				precision;
+	int				length;
+	char			type;
+	const char		*format_end;
+}					t_specs;
 
 #endif
--- sources/ft_printf.c
+++ sources/ft_printf.c
@@ -6,7 +6,7 @@
 /*   By: ngragas <ngragas@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/12/03 21:35:04 by ngragas           #+#    #+#             */
-/*   Updated: 2020/12/28 23:01:10 by ngragas          ###   ########.fr       */
+/*   Updated: 2020/12/29 22:15:52 by ngragas          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,35 +14,92 @@
 
 #include <stdio.h>
 
-static int	is_valid(const char *format)
+static t_specs	get_specs_2(const char *format, t_specs specs)
 {
-	(void)format;
-	return (1);
+	const char	*types_list = "cspdiuxXnfeg";
+	const char	*type;
+
+	if (*format == 'h' && --specs.length && *++format == 'h')
+		specs.length-- && format++;
+	else if (*format == 'l' && ++specs.length && *++format == 'l')
+		specs.length++ && format++;
+	if ((type = ft_strchr(types_list, *format)))
+	{
+		specs.type = *type;
+		specs.format_end = ++format;
+		if (ft_strchr("diuxX", specs.type) && specs.precision)
+			specs.flags ^= FLAG_ZERO;
+		if (specs.width < 0)
+		{
+			specs.flags |= FLAG_MINUS;
+			specs.width *= -1;
+		}
+		if (specs.flags & (FLAG_ZERO + FLAG_MINUS))
+			specs.flags ^= FLAG_ZERO;
+		if (specs.flags & (FLAG_SPACE + FLAG_PLUS))
+			specs.flags ^= FLAG_SPACE;
+	}
+	return (specs);
 }
 
-static int	parse_format(va_list ap, char *res, const char *format, int count)
+static t_specs	get_specs_1(va_list ap, const char *format)
 {
-	const char	*flag_list = "-+ #0";
-	const char	*size_list[] = {"hh", "h", "l", "ll", NULL};
-	const char	*types_list = "cspdiuxXnfge";
-	(void)flag_list; (void)size_list; (void)types_list;
-	(void)res;
+	const char		*flag_list = "-+ 0#";
+	unsigned char	flag;
+	t_specs			specs;
 
-	if (!(is_valid(format + count)))
+	specs = (t_specs){0, 0, 0, 0, '\0', NULL};
+	while ((flag = (ft_strchr(flag_list, *format) - flag_list)))
+		(specs.flags |= 1 << flag) && format++;
+	specs.width = (*format == '*' ? va_arg(ap, int) : ft_atoi(format));
+	if (*format != '*')
+		while (ft_isdigit(*format))
+			format++;
+	else
+		format++;
+	if (*format == '.')
+	{
+		specs.precision = *++format == '*' ? va_arg(ap, int) : ft_atoi(format);
+		if (specs.precision < 0)
+			specs.precision = 0;
+		if (*format != '*')
+			while (ft_isdigit(*format))
+				format++;
+		else
+			format++;
+	}
+	return (get_specs_2(format, specs));
+}
+
+static int		reformat(va_list ap, char *res, const char **format, int count)
+{
+	t_specs	specs;
+
+	specs = get_specs_1(ap, *format);
+	if (!(specs.type))
 		return (count);
-
-	if (*format == 's')
-		printf("arg = %s\n", va_arg(ap, char *));
-	else if (*format == 'd')
-		printf("arg = %d\n", va_arg(ap, int));
+	*format = specs.format_end;
+	res[0] = 0;
+	if (specs.type == 'c')
+		count += reformat_char(res, ap, specs);
+	else if (specs.type == 's')
+		count += reformat_string(res, ap, specs);
+	else if (specs.type == 'd' || specs.type == 'u' || specs.type == 'i')
+		count += reformat_integer(res, ap, specs);
+	else if (specs.type == 'p' || specs.type == 'x' || specs.type == 'X')
+		count += reformat_hex(res, ap, specs);
+	else if (specs.type == 'f' || specs.type == 'e' || specs.type == 'g')
+		count += reformat_float(res, ap, specs);
+	else if (specs.type == 'n')
+		put_count(va_arg(ap, int *), count, specs);
 	return (count);
 }
 
-int			ft_printf(const char *format, ...)
+int				ft_printf(const char *format, ...)
 {
-	va_list	ap;
-	char 	res[1000];
+	char 	res[4096];
 	int		char_count;
+	va_list	ap;
 
 	va_start(ap, format);
 	char_count = 0;
@@ -51,7 +108,7 @@ int			ft_printf(const char *format, ...)
 		if (*format != '%')
 			res[char_count++] = *format++;
 		else if (*++format && *format != '%')
-			char_count = parse_format(ap, res, format, char_count);
+			char_count = reformat(ap, res + char_count, &format, char_count);
 		else
 			res[char_count++] = '%';
 	}
