19ac6459ccb5a92fbadda403f5682a77d0664e94
ngragas <ngragas@student.21-school.ru>
2020-12-30 22:55:53 +0300

    parsing v1 finished

--- includes/ft_printf.h
+++ includes/ft_printf.h
@@ -6,7 +6,7 @@
 /*   By: ngragas <ngragas@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/12/03 21:34:53 by ngragas           #+#    #+#             */
-/*   Updated: 2020/12/29 22:03:56 by ngragas          ###   ########.fr       */
+/*   Updated: 2020/12/30 19:20:18 by ngragas          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,6 @@
 # define FT_PRINTF_H
 
 # include "libft.h"
-
 # include <unistd.h>
 # include <stdarg.h>
 
@@ -22,22 +21,23 @@ int					ft_printf(const char *format, ...);
 
 /*
 ** length: 'hh' = -2; 'h' = -1; 'l' = 1; 'll' = 2
-**	flags:	'-'	= 1		'+'	= 2		' '	= 4		'0'	= 8		'#'	= 16
-**				= 00001		= 00010		= 00100		= 01000		= 10000
+**	flags:	'-'	=1		'+'	=2		' '	=4		'0'	=8		'#'	=16		'.' = 32
+**				=000001		=000010		=000100		=001000		=010000	 =100000
 */
 
-# define FLAG_MINUS	1
-# define FLAG_PLUS	2
-# define FLAG_SPACE	4
-# define FLAG_ZERO	8
-# define FLAG_HASH	16
+# define FLAG_MINUS		1
+# define FLAG_PLUS		2
+# define FLAG_SPACE		4
+# define FLAG_ZERO		8
+# define FLAG_HASH		16
+# define FLAG_PRECISION	32
 
 typedef struct		s_specs
 {
 	unsigned char	flags;
 	int				width;
 	int				precision;
-	int				length;
+	char			length;
 	char			type;
 	const char		*format_end;
 }					t_specs;
--- sources/ft_printf.c
+++ sources/ft_printf.c
@@ -6,98 +6,120 @@
 /*   By: ngragas <ngragas@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/12/03 21:35:04 by ngragas           #+#    #+#             */
-/*   Updated: 2020/12/29 22:15:52 by ngragas          ###   ########.fr       */
+/*   Updated: 2020/12/30 22:52:57 by ngragas          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
 
-#include <stdio.h>
+#include <stdio.h> // !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+
+int	ft_atoi_shift(const char **src)
+{
+	const char	*str = *src;
+	int			num;
+
+	num = 0;
+	while ('0' <= *str && *str <= '9')
+		num = num * 10 + (*str++ - '0');
+	*src = str;
+	return (num);
+}
 
 static t_specs	get_specs_2(const char *format, t_specs specs)
 {
-	const char	*types_list = "cspdiuxXnfeg";
-	const char	*type;
-
 	if (*format == 'h' && --specs.length && *++format == 'h')
 		specs.length-- && format++;
 	else if (*format == 'l' && ++specs.length && *++format == 'l')
 		specs.length++ && format++;
-	if ((type = ft_strchr(types_list, *format)))
+	specs.type = *format;
+	specs.format_end = format;
+	if (ft_strchr("diuxX", specs.type) && (specs.flags & FLAG_PRECISION))
+		specs.flags &= ~FLAG_ZERO;
+	if (specs.type && !(specs.flags & FLAG_PRECISION))
 	{
-		specs.type = *type;
-		specs.format_end = ++format;
-		if (ft_strchr("diuxX", specs.type) && specs.precision)
-			specs.flags ^= FLAG_ZERO;
-		if (specs.width < 0)
-		{
-			specs.flags |= FLAG_MINUS;
-			specs.width *= -1;
-		}
-		if (specs.flags & (FLAG_ZERO + FLAG_MINUS))
-			specs.flags ^= FLAG_ZERO;
-		if (specs.flags & (FLAG_SPACE + FLAG_PLUS))
-			specs.flags ^= FLAG_SPACE;
+		if (ft_strchr("feg", specs.type))
+			specs.precision = 6;
+		else if (ft_strchr("feg", specs.type))
+			specs.precision = 1;
 	}
+	if (specs.width < 0)
+	{
+		specs.flags |= FLAG_MINUS;
+		specs.width *= -1;
+	}
+	if (specs.flags & FLAG_MINUS)
+		specs.flags &= ~FLAG_ZERO;
+	if (specs.flags & FLAG_PLUS)
+		specs.flags &= ~FLAG_SPACE;
 	return (specs);
 }
 
 static t_specs	get_specs_1(va_list ap, const char *format)
 {
-	const char		*flag_list = "-+ 0#";
-	unsigned char	flag;
-	t_specs			specs;
+	const char	*flag_list = "-+ 0#";
+	const char	*flag;
+	t_specs		specs;
 
-	specs = (t_specs){0, 0, 0, 0, '\0', NULL};
-	while ((flag = (ft_strchr(flag_list, *format) - flag_list)))
-		(specs.flags |= 1 << flag) && format++;
-	specs.width = (*format == '*' ? va_arg(ap, int) : ft_atoi(format));
-	if (*format != '*')
-		while (ft_isdigit(*format))
-			format++;
-	else
+	specs = (t_specs){0, 0, 1, 0, '\0', NULL};
+	while ((flag = ft_strchr(flag_list, *++format)))
+		specs.flags |= (1 << (flag - flag_list));
+	specs.width = (*format == '*' ? va_arg(ap, int) : ft_atoi_shift(&format));
+	if (*format == '*')
 		format++;
 	if (*format == '.')
 	{
-		specs.precision = *++format == '*' ? va_arg(ap, int) : ft_atoi(format);
-		if (specs.precision < 0)
-			specs.precision = 0;
-		if (*format != '*')
-			while (ft_isdigit(*format))
-				format++;
-		else
+		specs.precision = *++format == '*' ? \
+									va_arg(ap, int) : ft_atoi_shift(&format);
+		if (specs.precision >= 0)
+			specs.flags |= FLAG_PRECISION;
+		if (*format == '*')
 			format++;
 	}
 	return (get_specs_2(format, specs));
 }
 
-static int		reformat(va_list ap, char *res, const char **format, int count)
+static int		reformat(char *res, va_list ap, const char **format, int count)
 {
 	t_specs	specs;
 
 	specs = get_specs_1(ap, *format);
-	if (!(specs.type))
-		return (count);
 	*format = specs.format_end;
-	res[0] = 0;
-	if (specs.type == 'c')
-		count += reformat_char(res, ap, specs);
-	else if (specs.type == 's')
-		count += reformat_string(res, ap, specs);
-	else if (specs.type == 'd' || specs.type == 'u' || specs.type == 'i')
-		count += reformat_integer(res, ap, specs);
-	else if (specs.type == 'p' || specs.type == 'x' || specs.type == 'X')
-		count += reformat_hex(res, ap, specs);
-	else if (specs.type == 'f' || specs.type == 'e' || specs.type == 'g')
-		count += reformat_float(res, ap, specs);
-	else if (specs.type == 'n')
-		put_count(va_arg(ap, int *), count, specs);
+	if (**format)
+		(*format)++;
+	*res = '@';
+	count++;
+
+	printf("\nspecs.type = %c", specs.type);
+	printf("\nspecs.width = %d", specs.width);
+	printf("\nspecs.precision = %d", specs.precision);
+	printf("\nspecs.length = %hhd", specs.length);
+	printf("\nspecs.flags = \"");
+	if (specs.flags & FLAG_MINUS) printf("-");
+	if (specs.flags & FLAG_PLUS) printf("+");
+	if (specs.flags & FLAG_SPACE) printf(" ");
+	if (specs.flags & FLAG_ZERO) printf("0");
+	if (specs.flags & FLAG_HASH) printf("#");
+	if (specs.flags & FLAG_PRECISION) printf(".");
+	printf("\"\n");
+//	if (specs.type == 's')
+//		count += reformat_string(res, ap, specs);
+//	else if (specs.type == 'd' || specs.type == 'u' || specs.type == 'i')
+//		count += reformat_integer(res, ap, specs);
+//	else if (specs.type == 'p' || specs.type == 'x' || specs.type == 'X')
+//		count += reformat_hex(res, ap, specs);
+//	else if (specs.type == 'f' || specs.type == 'e' || specs.type == 'g')
+//		count += reformat_float(res, ap, specs);
+//	else if (specs.type == 'n')
+//		put_count(va_arg(ap, int *), count, specs);
+//	else if (specs.type == 'c' || specs.type)
+//		count += reformat_char(res, ap, specs);
 	return (count);
 }
 
 int				ft_printf(const char *format, ...)
 {
-	char 	res[4096];
+	char 	res[65536];
 	int		char_count;
 	va_list	ap;
 
@@ -107,10 +129,8 @@ int				ft_printf(const char *format, ...)
 	{
 		if (*format != '%')
 			res[char_count++] = *format++;
-		else if (*++format && *format != '%')
-			char_count = reformat(ap, res + char_count, &format, char_count);
-		else
-			res[char_count++] = '%';
+		else if (*format)
+			char_count = reformat(res + char_count, ap, &format, char_count);
 	}
 	write(1, res, char_count);
 	va_end(ap);
