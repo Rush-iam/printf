503b1e89ad0fa695dcf7398c0475567925b20aa9
ngragas <ngragas@student.21-school.ru>
2021-01-04 23:04:34 +0300

    string specifier done

--- includes/ft_printf.h
+++ includes/ft_printf.h
@@ -6,7 +6,7 @@
 /*   By: ngragas <ngragas@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/12/03 21:34:53 by ngragas           #+#    #+#             */
-/*   Updated: 2020/12/31 00:01:07 by ngragas          ###   ########.fr       */
+/*   Updated: 2021/01/04 13:31:31 by ngragas          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,20 +23,20 @@ int					ft_printf(const char *format, ...);
 **	flags:	'-'	=1		'+'	=2		' '	=4		'0'	=8		'#'	=16		'.' = 32
 **				=000001		=000010		=000100		=001000		=010000	 =100000
 ** length: 'hh' =1		'h' =3		'l' =7		'll'=15
-* 				=0001		=0011		=0111		=1111
+**				=0001		=0011		=0111		=1111
 */
 
-# define FLAG_MINUS		1
-# define FLAG_PLUS		2
-# define FLAG_SPACE		4
-# define FLAG_ZERO		8
-# define FLAG_HASH		16
-# define FLAG_PRECISION	32
+# define FLAG_MINUS		0b000001
+# define FLAG_PLUS		0b000010
+# define FLAG_SPACE		0b000100
+# define FLAG_ZERO		0b001000
+# define FLAG_HASH		0b010000
+# define FLAG_PRECISION	0b100000
 
-# define LEN_HH	1
-# define LEN_H	3
-# define LEN_L	7
-# define LEN_LL	15
+# define LEN_HH	0b0001
+# define LEN_H	0b0011
+# define LEN_L	0b0111
+# define LEN_LL	0b1111
 
 typedef struct		s_specs
 {
@@ -45,7 +45,6 @@ typedef struct		s_specs
 	int				precision;
 	char			len;
 	char			type;
-	const char		*format_end;
 }					t_specs;
 
 #endif
--- sources/ft_printf.c
+++ sources/ft_printf.c
@@ -6,7 +6,7 @@
 /*   By: ngragas <ngragas@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/12/03 21:35:04 by ngragas           #+#    #+#             */
-/*   Updated: 2020/12/31 00:14:20 by ngragas          ###   ########.fr       */
+/*   Updated: 2021/01/04 22:58:34 by ngragas          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,11 +14,142 @@
 
 #include <stdio.h> // !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 
-int	atoi_shift(const char **str)
+size_t			ft_wstrlen(const wchar_t *s, ssize_t n)
+{
+	int		char_size;
+	size_t	count;
+
+	count = 0;
+	while (*s && (n == -1 || count < n))
+	{
+		if (*s >= 65536)
+			char_size = 4;
+		else if (*s >= 2048)
+			char_size = 3;
+		else if (*s >= 128)
+			char_size = 2;
+		else
+			char_size = 1;
+		if (n != -1 && count + char_size > n)
+			break ;
+		count += char_size;
+		s++;
+	}
+	return (count);
+}
+
+int				ft_wchrtoutf8(char *dst, const wchar_t src)
+{
+	int	bytes;
+
+	bytes = 1;
+	if (src < 128)
+		*dst = (char)src;
+	else if (src < 2048)
+	{
+		dst[0] = (char)(0b11000000 | src >> 6);
+		(dst[1] = (char)(0b10000000 | src & 0b111111)) && (bytes++);
+	}
+	else if (src < 65536)
+	{
+		dst[0] = (char)(0b11100000 | src >> 12);
+		dst[1] = (char)(0b10000000 | src >> 6 & 0b111111);
+		(dst[2] = (char)(0b10000000 | src & 0b111111)) && (bytes += 2);
+	}
+	else if (src < 1114112)
+	{
+		dst[0] = (char)(0b11110000 | src >> 18);
+		dst[1] = (char)(0b10000000 | src >> 12 & 0b111111);
+		dst[2] = (char)(0b10000000 | src >> 6 & 0b111111);
+		(dst[3] = (char)(0b10000000 | src & 0b111111)) && (bytes += 3);
+	}
+	return (bytes);
+}
+
+size_t			ft_wstrtoutf8(char *dst, const wchar_t *src, size_t n)
+{
+	int		char_size;
+	size_t	count;
+
+	count = 0;
+	while (*src && count < n)
+	{
+		if (*src >= 65536)
+			char_size = 4;
+		else if (*src >= 2048)
+			char_size = 3;
+		else if (*src >= 128)
+			char_size = 2;
+		else
+			char_size = 1;
+		if (count + char_size > n)
+			break ;
+		count += char_size;
+		dst += ft_wchrtoutf8(dst, *src);
+		src++;
+	}
+	*dst = '\0';
+	return (count);
+}
+
+size_t			reformat_string_utf8(char *res, wchar_t *string, t_specs specs)
+{
+	const size_t	src_len = ft_wstrlen(string, -1);
+	size_t			dst_len;
+	size_t			real_len;
+
+	if ((specs.flags & FLAG_PRECISION) && specs.precision < src_len)
+		dst_len = ft_wstrlen(string, specs.precision);
+	else
+		dst_len = src_len;
+	real_len = (specs.width > dst_len ? specs.width : dst_len);
+	if (specs.flags & FLAG_MINUS)
+	{
+		ft_wstrtoutf8(res, string, dst_len);
+		res += dst_len;
+		ft_memset(res, ' ', real_len - dst_len);
+	}
+	else
+	{
+		ft_memset(res, ' ', real_len - dst_len);
+		res += real_len - dst_len;
+		ft_wstrtoutf8(res, string, dst_len);
+	}
+	return (real_len);
+}
+
+size_t			reformat_string(char *res, char *string, t_specs specs)
+{
+	const size_t	src_len = ft_strlen(string);
+	size_t			dst_len;
+	size_t			real_len;
+
+	if ((specs.flags & FLAG_PRECISION) && specs.precision < src_len)
+		dst_len = specs.precision;
+	else
+		dst_len = src_len;
+	real_len = (specs.width > dst_len ? specs.width : dst_len);
+	if (specs.flags & FLAG_MINUS)
+	{
+		ft_memcpy(res, string, dst_len);
+		res += dst_len;
+		ft_memset(res, ' ', real_len - dst_len);
+	}
+	else
+	{
+		ft_memset(res, ' ', real_len - dst_len);
+		res += real_len - dst_len;
+		ft_memcpy(res, string, dst_len);
+	}
+	return (real_len);
+}
+
+static int		atoi_shift(const char **str)
 {
 	int	num;
 
 	num = 0;
+	(*str)--;
 	while ('0' <= **str && **str <= '9')
 	{
 		num = num * 10 + (**str - '0');
@@ -27,9 +158,9 @@ int	atoi_shift(const char **str)
 	return (num);
 }
 
-const char	*get_specs_2(const char *format, t_specs *specs)
+const char		*get_specs_2(const char *fstr, t_specs *specs)
 {
-	specs->type = *format;
+	specs->type = *fstr;
 	if (ft_strchr("diuxX", specs->type) && (specs->flags & FLAG_PRECISION))
 		specs->flags &= ~FLAG_ZERO;
 	if (specs->precision < 0 && (specs->flags & FLAG_PRECISION))
@@ -50,48 +181,46 @@ const char	*get_specs_2(const char *format, t_specs *specs)
 		specs->flags &= ~FLAG_ZERO;
 	if (specs->flags & FLAG_PLUS)
 		specs->flags &= ~FLAG_SPACE;
-	return (format);
+	return (fstr);
 }
 
-const char	*get_specs_1(va_list ap, const char *frmt, t_specs *s)
+const char		*get_specs_1(va_list ap, const char *fstr, t_specs *s)
 {
-	const char	*fields_list = "-+ 0#.lh*123456789";
+	const char	fields_list[] = "-+ 0#.lh*123456789";
 	const char	*flag;
 
-	while (*frmt && (flag = ft_memchr(fields_list, *frmt, 18)))
+	while (*fstr && (flag = ft_memchr(fields_list, *fstr, sizeof(fields_list))))
 	{
 		if (flag - fields_list < 5)
 		{
-			(s->flags |= (1 << (flag - fields_list))) && frmt++;
+			s->flags |= (1 << (flag - fields_list));
+			fstr++;
 			continue;
 		}
-		if (*frmt == '*' || ft_isdigit(*frmt))
-			s->width = (*frmt == '*' ? va_arg(ap, int) : atoi_shift(&frmt));
-		*frmt == '*' ? frmt++ : 0;
-		if (*frmt == '.')
+		if (*fstr == '*' || ft_isdigit(*fstr))
+			s->width = (*fstr++ == '*' ? va_arg(ap, int) : atoi_shift(&fstr));
+		if (*fstr == '.')
 		{
-			s->precision = *++frmt == '*' ? va_arg(ap, int) : atoi_shift(&frmt);
+			fstr++;
+			s->precision = *fstr++ == '*' ? va_arg(ap, int) : atoi_shift(&fstr);
 			s->precision >= 0 ? s->flags |= FLAG_PRECISION : 0;
-			*frmt == '*' ? frmt++ : 0;
 		}
-		if (*frmt == 'h')
-			*++frmt == 'h' ? (s->len |= LEN_HH) && frmt++ : (s->len |= LEN_H);
-		else if (*frmt == 'l')
-			*++frmt == 'l' ? (s->len |= LEN_LL) && frmt++ : (s->len |= LEN_L);
+		if (*fstr == 'h')
+			*++fstr == 'h' ? (s->len |= LEN_HH) && fstr++ : (s->len |= LEN_H);
+		else if (*fstr == 'l')
+			*++fstr == 'l' ? (s->len |= LEN_LL) && fstr++ : (s->len |= LEN_L);
 	}
-	return (get_specs_2(frmt, s));
+	return (get_specs_2(fstr, s));
 }
 
-static int		reformat(char *res, va_list ap, const char **format, int count)
+static int		reformat(char *res, va_list ap, const char **fstr, int count)
 {
 	t_specs	specs;
 
-	specs = (t_specs){0, 0, 0, 0, '\0', NULL};
-	*format = get_specs_1(ap, *format, &specs);
-	if (**format)
-		(*format)++;
-	*res = '@';
-	count++;
+	specs = (t_specs){0, 0, 0, 0, '\0'};
+	*fstr = get_specs_1(ap, *fstr, &specs);
+	if (**fstr)
+		(*fstr)++;
 
 	printf("\nspecs.type = %c", specs.type);
 	printf("\nspecs.width = %d", specs.width);
@@ -105,8 +234,11 @@ static int		reformat(char *res, va_list ap, const char **format, int count)
 	if (specs.flags & FLAG_HASH) printf("#");
 	if (specs.flags & FLAG_PRECISION) printf(".");
 	printf("\"\n");
-//	if (specs.type == 's')
-//		count += reformat_string(res, ap, specs);
+
+	if (specs.type == 's' && specs.len != LEN_L)
+		count += reformat_string(res, va_arg(ap, char *), specs);
+	else if (specs.type == 's' && specs.len == LEN_L)
+		count += reformat_string_utf8(res, va_arg(ap, wchar_t *), specs);
 //	else if (specs.type == 'd' || specs.type == 'u' || specs.type == 'i')
 //		count += reformat_integer(res, ap, specs);
 //	else if (specs.type == 'p' || specs.type == 'x' || specs.type == 'X')
