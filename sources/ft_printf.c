38a54ad0ddebf70386d01eec2312d4a6ae0e8314
ngragas <ngragas@student.21-school.ru>
2021-01-07 18:15:57 +0300

    separate file for parsing functions

--- sources/ft_printf.c
+++ sources/ft_printf.c
@@ -6,48 +6,11 @@
 /*   By: ngragas <ngragas@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/12/03 21:35:04 by ngragas           #+#    #+#             */
-/*   Updated: 2021/01/07 17:53:08 by ngragas          ###   ########.fr       */
+/*   Updated: 2021/01/07 17:55:28 by ngragas          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
-
-void		ft_printf_bufcpy(t_buf *buf, const char *src, size_t n)
-{
-	if (buf->len + n > sizeof(buf->str))
-	{
-		write(STDOUT_FILENO, buf->str, buf->len);
-		buf->len = 0;
-		if (n > sizeof(buf->str))
-		{
-			write(STDOUT_FILENO, src, n);
-			n = 0;
-		}
-	}
-	while (n--)
-		buf->str[buf->len++] = *src++;
-}
-
-void		ft_printf_bufset(t_buf *buf, char c, size_t n)
-{
-	int	i;
-
-	if (buf->len + n >= sizeof(buf->str))
-	{
-		write(STDOUT_FILENO, buf->str, buf->len);
-		buf->len = 0;
-		while (n >= sizeof(buf->str))
-		{
-			i = 0;
-			while (i < (int)sizeof(buf->str))
-				buf->str[i++] = c;
-			write(STDOUT_FILENO, buf->str, sizeof(buf->str));
-			n -= sizeof(buf->str);
-		}
-	}
-	while (n--)
-		buf->str[buf->len++] = c;
-}
 
 int			ft_printf(const char *format, ...)
 {
@@ -120,74 +83,39 @@ int			ft_printf_format(t_buf *res, va_list ap, const char **format)
 	return (count);
 }
 
-const char	*ft_printf_parse_specs_1(va_list ap, const char *fstr, t_specs *s)
+void		ft_printf_bufcpy(t_buf *buf, const char *src, size_t n)
 {
-	const char	fields_list[] = "-+ 0#.lh*123456789";
-	const char	*flag;
-
-	while (*fstr && (flag = ft_memchr(fields_list, *fstr, sizeof(fields_list))))
+	if (buf->len + n > sizeof(buf->str))
 	{
-		if (flag - fields_list < 5 && fstr++)
+		write(STDOUT_FILENO, buf->str, buf->len);
+		buf->len = 0;
+		if (n > sizeof(buf->str))
 		{
-			s->flags = (char)(s->flags | 1 << (flag - fields_list));
-			continue;
+			write(STDOUT_FILENO, src, n);
+			n = 0;
 		}
-		if (*fstr == '*' || ft_isdigit(*fstr))
-			s->width = (*fstr++ == '*') ?
-						va_arg(ap, int) : ft_printf_parse_atoi(&fstr);
-		if (*fstr == '.' && fstr++)
-		{
-			s->precision = (*fstr++ == '*') ?
-						va_arg(ap, int) : ft_printf_parse_atoi(&fstr);
-			s->precision >= 0 ? s->flags |= FLAG_PRECISION : 0;
-		}
-		if (*fstr == 'h')
-			*++fstr == 'h' ? (s->len |= LEN_HH) && fstr++ : (s->len |= LEN_H);
-		else if (*fstr == 'l')
-			*++fstr == 'l' ? (s->len |= LEN_LL) && fstr++ : (s->len |= LEN_L);
 	}
-	return (ft_printf_parse_specs_2(fstr, s));
+	while (n--)
+		buf->str[buf->len++] = *src++;
 }
 
-const char	*ft_printf_parse_specs_2(const char *fstr, t_specs *specs)
+void		ft_printf_bufset(t_buf *buf, char c, size_t n)
 {
-	specs->type = *fstr;
-	if (specs->type == '\0')
-		return (fstr);
-	if (specs->type == 'p')
-		specs->len = LEN_L;
-	if (ft_strchr("diuxX", specs->type) && (specs->flags & FLAG_PRECISION))
-		specs->flags &= ~FLAG_ZERO;
-	if (specs->precision < 0 || (specs->flags & FLAG_PRECISION) == 0)
-	{
-		specs->flags &= ~FLAG_PRECISION;
-		if (ft_strchr("feg", specs->type))
-			specs->precision = 6;
-		else if (ft_strchr("diuxXp", specs->type))
-			specs->precision = 1;
-	}
-	if (specs->width < 0)
-	{
-		specs->flags |= FLAG_MINUS;
-		specs->width *= -1;
-	}
-	if (specs->flags & FLAG_MINUS)
-		specs->flags &= ~FLAG_ZERO;
-	if (specs->flags & FLAG_PLUS)
-		specs->flags &= ~FLAG_SPACE;
-	return (fstr);
-}
+	int	i;
 
-int			ft_printf_parse_atoi(const char **str)
-{
-	int	num;
-
-	num = 0;
-	(*str)--;
-	while ('0' <= **str && **str <= '9')
+	if (buf->len + n >= sizeof(buf->str))
 	{
-		num = num * 10 + (**str - '0');
-		(*str)++;
+		write(STDOUT_FILENO, buf->str, buf->len);
+		buf->len = 0;
+		while (n >= sizeof(buf->str))
+		{
+			i = 0;
+			while (i < (int)sizeof(buf->str))
+				buf->str[i++] = c;
+			write(STDOUT_FILENO, buf->str, sizeof(buf->str));
+			n -= sizeof(buf->str);
+		}
 	}
-	return (num);
+	while (n--)
+		buf->str[buf->len++] = c;
 }
---
+++ sources/ft_printf_parser.c
@@ -0,0 +1,85 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_printf_parser.c                                 :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ngragas <ngragas@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/01/07 17:58:31 by ngragas           #+#    #+#             */
+/*   Updated: 2021/01/07 18:00:06 by ngragas          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "ft_printf.h"
+
+const char	*ft_printf_parse_specs_1(va_list ap, const char *fstr, t_specs *s)
+{
+	const char	fields_list[] = "-+ 0#.lh*123456789";
+	const char	*flag;
+
+	while (*fstr && (flag = ft_memchr(fields_list, *fstr, sizeof(fields_list))))
+	{
+		if (flag - fields_list < 5 && fstr++)
+		{
+			s->flags = (char)(s->flags | 1 << (flag - fields_list));
+			continue;
+		}
+		if (*fstr == '*' || ft_isdigit(*fstr))
+			s->width = (*fstr++ == '*') ?
+						va_arg(ap, int) : ft_printf_parse_atoi(&fstr);
+		if (*fstr == '.' && fstr++)
+		{
+			s->precision = (*fstr++ == '*') ?
+						va_arg(ap, int) : ft_printf_parse_atoi(&fstr);
+			s->precision >= 0 ? s->flags |= FLAG_PRECISION : 0;
+		}
+		if (*fstr == 'h')
+			*++fstr == 'h' ? (s->len |= LEN_HH) && fstr++ : (s->len |= LEN_H);
+		else if (*fstr == 'l')
+			*++fstr == 'l' ? (s->len |= LEN_LL) && fstr++ : (s->len |= LEN_L);
+	}
+	return (ft_printf_parse_specs_2(fstr, s));
+}
+
+const char	*ft_printf_parse_specs_2(const char *fstr, t_specs *specs)
+{
+	specs->type = *fstr;
+	if (specs->type == '\0')
+		return (fstr);
+	if (specs->type == 'p')
+		specs->len = LEN_L;
+	if (ft_strchr("diuxX", specs->type) && (specs->flags & FLAG_PRECISION))
+		specs->flags &= ~FLAG_ZERO;
+	if (specs->precision < 0 || (specs->flags & FLAG_PRECISION) == 0)
+	{
+		specs->flags &= ~FLAG_PRECISION;
+		if (ft_strchr("feg", specs->type))
+			specs->precision = 6;
+		else if (ft_strchr("diuxXp", specs->type))
+			specs->precision = 1;
+	}
+	if (specs->width < 0)
+	{
+		specs->flags |= FLAG_MINUS;
+		specs->width *= -1;
+	}
+	if (specs->flags & FLAG_MINUS)
+		specs->flags &= ~FLAG_ZERO;
+	if (specs->flags & FLAG_PLUS)
+		specs->flags &= ~FLAG_SPACE;
+	return (fstr);
+}
+
+int			ft_printf_parse_atoi(const char **str)
+{
+	int	num;
+
+	num = 0;
+	(*str)--;
+	while ('0' <= **str && **str <= '9')
+	{
+		num = num * 10 + (**str - '0');
+		(*str)++;
+	}
+	return (num);
+}
